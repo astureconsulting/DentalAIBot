880f97a06e79f995ad9e87dc2524a6024bea912c
astureconsulting <post@asture.co>
2025-07-04 16:37:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -309,7 +309,7 @@ def chat():
             "groq_response": response.text
         }), 500
         
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
